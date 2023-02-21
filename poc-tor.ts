1b0cf1d2ad538371ed27ff99bbc0caca7bec65c9
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2023-02-21 14:29:11 +0100

    Create poc-tor.ts

---
+++ poc-tor.ts
@@ -0,0 +1,32 @@
+/**
+ *        Solidarity Freedom Unit
+ *           Derived from Pix4
+ *  
+ *         Proof Of Concept project
+ *              @z3ntl3
+ */
+import Undici, { ProxyAgent, setGlobalDispatcher } from "undici";
+import { Writable } from "stream";
+const {XMLParser} = require('fast-xml-parser');
+const options = {
+  ignoreAttributes : false
+};
+
+const parser = new XMLParser(options);
+const agent = new ProxyAgent('http://127.0.0.1:8118') // default docker image env port
+setGlobalDispatcher(agent);
+
+(async () => {
+  let buffer = ""
+  await Undici.stream('https://pool.proxyspace.pro/judge.php', 
+  {method: 'GET', headers: {'cache-control':'no-cache'}},(data)=>{ 
+    return new Writable({
+        write: (chunk, encoding, callback) => {
+            buffer += chunk
+            callback()
+        }
+    })
+})
+  let json = parser.parse(buffer);
+  console.log(`\x1b[1m${json.body.link.pre}\x1b[0m`)
+})()
